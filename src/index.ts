4c76ecf42c14fbcd2cbc6fbf7c1637bb6a05d788
conner-calhoun <conner610@gmail.com>
2020-07-25 10:36:25 -0500

    add query classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,57 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`)
+/*
+
+Here are some useful URLs
+
+-- Get the page for durability (stills needs scraping)
+https://rustlabs.com/item/{itemName}#tab=destroyed-by;filter=0,1,1,1,1;sort=4,0,3
+
+
+*/
+
+// Types of queries that can be made
+enum QueryType {
+    Durability
 }
-hello('TypeScript')
+
+// Base Query Class
+class Query {
+    query: string
+
+    // Empty constructor
+    constructor() {
+        this.query = ""
+    }
+
+    // Execute the query
+    run() {
+
+    }
+
+    // Return the query as a string
+    getQuery(): string {
+        return this.query;
+    }
+}
+
+// Durability Query
+class DurabilityQuery extends Query {
+    constructor(itemName: string) {
+        super()
+        this.query = `https://rustlabs.com/item/${itemName}#tab=destroyed-by;filter=0,1,1,1,1;sort=4,0,3`
+    }
+}
+
+// Helper function to build the query object
+function buildQuery(queryType: QueryType, itemName: string): Query {
+    if (queryType == QueryType.Durability) {
+        return new DurabilityQuery(itemName)
+    } else {
+        return new Query()
+    }
+}
+
+function main() {
+    var armoredDoor: Query = buildQuery(QueryType.Durability, 'armored-door');
+    console.log(armoredDoor.getQuery());
+}
+main()
